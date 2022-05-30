0ebb012504f30b43797c4d208ce04df8bceb4c07
Нина Кесарева <n1n@list.ru>
2022-05-30 19:30:04 +0300

    task3

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int account = 130;
-        int add = 1500;
-        int bonus;
-        if (add >= 1000) {
-            bonus = add / 100;
-        } else {
-            bonus = 0;
-        }
-        int bill = (account + add + bonus);
-        System.out.println(bill);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
